302af2056a08711ddffd242efc36075a9e10f316
Marya Doery <mdoery@gmail.com>
2020-08-01 21:18:59 -0400

    Add cart-service for view of cart etc

---
+++ src/cart-service/cart-service.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'http://localhost:8000';
+
+export function addToCart(plate) {
+	fetch(BASE_URL + '/cart/' + plate._id, {method: 'PUT'})
+	.then(response => response.json())
+	.then(data => {});
+	return true;
+}
+
+export async function getCartContents(plate) {
+	let output = {};
+	await fetch(BASE_URL + '/cart', {method: 'GET'})
+	.then(response => response.json())
+	.then(data => { output = data; });
+	return output;
+};
+
+export function removeFromCart(plate) {
+	fetch(BASE_URL + '/cart/' + plate._id, {method: 'DELETE'});
+}
