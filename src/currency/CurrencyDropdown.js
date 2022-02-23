48e7cf40045da33804b095a0dd8b29928b4f57ab
alcfeoh <alain@interstate21.com>
2022-02-23 10:53:42 -0800

    Fixed CurrencyDropdown component

--- src/currency/CurrencyDropdown.js
+++ src/currency/CurrencyDropdown.js
@@ -12,11 +12,20 @@ class CurrencyDropdown extends React.Component {
 	render() {
 		return (
 			<div className="btn-group margin10">
-				<button type="button" className="btn btn-info dropdown-toggle" data-toggle="dropdown">{this.state.currency}</button>
+				<button type="button" onClick={() => this.setState({showItems: true})}
+					className="btn btn-info dropdown-toggle" data-toggle="dropdown">
+					{this.state.currency}
+				</button>
 				<div className={this.state.showItems? "dropdown-menu show" : "dropdown-menu"}>
-				<a className="dropdown-item">USD ($)</a>
-				<a className="dropdown-item">EUR (€)</a>
-				<a className="dropdown-item">GBP (£)</a>
+					<a className="dropdown-item" onClick={() => this.setState({showItems: false, currency: 'USD'})}>
+						USD ($)
+					</a>
+					<a className="dropdown-item" onClick={() => this.setState({showItems: false, currency: 'EUR'})}>
+						EUR (€)
+					</a>
+					<a className="dropdown-item" onClick={() => this.setState({showItems: false, currency: 'GBP'})}>
+						GBP (£)
+					</a>
 				</div>
 			</div>
 		);
