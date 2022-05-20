a6338077ce886c9b9e7acff159effdf21736d02f
alcfeoh <alain@interstate21.com>
2022-05-20 15:26:14 -0700

    Moved cart service and pop-up window to the right place

--- lab-templates/cart-service/cart-service.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'http://localhost:8000';
-
-export function addToCart(plate) {
-	fetch('http://localhost:8000/cart/' + plate._id, {method: 'PUT'})
-	.then(response => response.json())
-	.then(data => {});
-	return true;
-}
