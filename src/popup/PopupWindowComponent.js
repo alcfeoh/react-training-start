381af801cff30a216fa1c49b5c49c5c432c67173
alcfeoh <alain@interstate21.com>
2022-05-20 15:34:14 -0700

    Changed name of popup-window prop

--- src/popup/PopupWindowComponent.js
+++ src/popup/PopupWindowComponent.js
@@ -14,7 +14,7 @@ class PopupWindowComponent extends React.Component {
 					<tbody>
 						<tr>
 							<td className="alignLeft" width="98%">Item added to cart</td>
-							<td className="alignRight" onClick={() => { this.props.close(); } }><sup>&#10005;</sup></td>
+							<td className="alignRight" onClick={() => { this.props.onClose(); } }><sup>&#10005;</sup></td>
 						</tr>
 					</tbody>
 				</table>
