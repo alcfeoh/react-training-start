a68ada2759bd128cfb37ccc0052be4f934c84ba6
alcfeoh <alain@interstate21.com>
2022-05-20 14:52:01 -0700

    Added Jumbotron unit test, removed failing App test

--- src/jumbotron/Jumbotron.test.js
+++ src/jumbotron/Jumbotron.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Jumbotron} from "./Jumbotron";
 import {render} from "@testing-library/react";
 
-test('renders learn react link', () => {
+test('renders Jumbotron with the proper title', () => {
     const { getByText } = render(<Jumbotron title="Welcome to our store"/>);
     const titleElement = getByText("Welcome to our store");
     expect(titleElement).toBeInTheDocument();
