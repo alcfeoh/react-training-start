9d368b45f28848e5ea2a39d867c2f6d4f2e55d3d
alcfeoh <alain@interstate21.com>
2022-05-20 15:40:51 -0700

    Proper cart-service.js

--- src/cart-service/cart-service.js
+++ src/cart-service/cart-service.js
@@ -1,20 +1,15 @@
 const BASE_URL = 'http://localhost:8000';
 
 export function addToCart(plate) {
-	fetch(BASE_URL + '/cart/' + plate._id, {method: 'PUT'})
-	.then(response => response.json())
-	.then(data => {});
-	return true;
+	return fetch(BASE_URL + '/cart/' + plate._id, {method: 'PUT'})
+	.then(response => response.json());
 }
 
-export async function getCartContents(plate) {
-	let output = {};
-	await fetch(BASE_URL + '/cart', {method: 'GET'})
-	.then(response => response.json())
-	.then(data => { output = data; });
-	return output;
+export function getCartContents(plate) {
+	return fetch(BASE_URL + '/cart', {method: 'GET'})
+	.then(response => response.json());
 };
 
 export function removeFromCart(plate) {
-	fetch(BASE_URL + '/cart/' + plate._id, {method: 'DELETE'});
+	return fetch(BASE_URL + '/cart/' + plate._id, {method: 'DELETE'});
 }
