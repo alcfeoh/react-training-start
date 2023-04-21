45d73ffe482ea170b2365290039ba1af17beeeb6
alcfeoh <alain@interstate21.com>
2023-04-21 11:29:50 -0700

    Updated LicensePlate to function component

--- src/license-plate/LicensePlate.js
+++ src/license-plate/LicensePlate.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 import './LicensePlate.css';
 
-export class LicensePlate extends Component {
+export function LicensePlate(props) {
 
-	render() {
-		const plate = this.props.plate;
-		return (
+	const {plate, buttonText} = props;
+
+	return (
 			<>
-			<h2>{plate.title}</h2>
-			<img src={plate.picture} className="img-fluid" />
-			<p>{plate.description}</p>
-			<div>
-			<h2 className="float-left">$xx</h2>
-			<button className="btn btn-primary float-right" role="button">
-				{this.props.buttonText}
-			</button>
-			</div>
+				<h2>{plate.title}</h2>
+				<img src={plate.picture} className="img-fluid" />
+				<p>{plate.description}</p>
+				<div>
+				<h2 className="float-left">$xx</h2>
+				<button className="btn btn-primary float-right" role="button">
+					{buttonText}
+				</button>
+				</div>
 			</>
 		);
 	}
