401ac6ea53be80f83a4dda68e55d1d085a4db63e
alcfeoh <alain@interstate21.com>
2023-06-23 15:57:16 -0700

    CheckoutForm is a function component

--- src/checkout-form/CheckoutForm.js
+++ src/checkout-form/CheckoutForm.js
@@ -1,80 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export class CheckoutForm extends React.Component {
+function CheckoutForm() {
 
-    state = {
-        "firstname": '',
-        "lastname": '',
-        "street": '',
-        "city": '',
-        "zip": '',
-        "zipValid": false,
-        "state": '',
-        "cc": ''
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [street, setStreet] = useState('');
+    const [city, setCity] = useState('');
+    const [zio, setZip] = useState('');
+    const [zipValid, setZipValid] = useState(false);
+    const [state, setState] = useState('');
+    const [cc, setCc] = useState('');
+
+    const handleChange = (event, setter) => {
+        const {value} = event.target;
+        setter(value);
     };
 
-    handleChange = (event) => {
-        const {name, value} = event.target;
-        this.setState({[name]: value });
-    };
+    return (
+        <form id="checkoutForm">
+          <div className="row">
+            <div className="col-lg-6">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="First name" name="firstname"
+                         required value={firstname} onChange={e => e.handleChange(e, setFirstname)}/>
+                </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Last name" name="lastname"
+                       required value={lastname} onChange={e => e.handleChange(e, setLastname)}/>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Street" name="street"/>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="City" name="city"/>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Zip" name="zip"/>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="State" name="state"/>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="input-group">
+                <input type="password" className="form-control" placeholder="Credit card number" name="cc" required/>
+              </div>
+            </div>
+            <div className="col-lg-6">
+            </div>
+          </div>
+          <br/>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
 
-    render() {
-		return (
-            <form id="checkoutForm">
-              <div className="row">
-                <div className="col-lg-6">
-                    <div className="input-group">
-                      <input type="text" className="form-control" placeholder="First name" name="firstname"
-                             required value={this.state.firstname} onChange={this.handleChange}/>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Last name" name="lastname"
-                           required value={this.state.lastname} onChange={this.handleChange}/>
-                  </div>
-                </div>
-              </div>
-              <br/>
-              <div className="row">
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Street" name="street"/>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="City" name="city"/>
-                  </div>
-                </div>
-              </div>
-              <br/>
-              <div className="row">
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Zip" name="zip"/>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" placeholder="State" name="state"/>
-                  </div>
-                </div>
-              </div>
-              <br/>
-              <div className="row">
-                <div className="col-lg-6">
-                  <div className="input-group">
-                    <input type="password" className="form-control" placeholder="Credit card number" name="cc" required/>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                </div>
-              </div>
-              <br/>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-
-		);
-	}
+    );
 }
