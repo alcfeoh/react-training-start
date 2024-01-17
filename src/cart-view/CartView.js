b96315db1204d019acacc0cc46f38338376e586a
alcfeoh <alain@interstate21.com>
2024-01-17 12:13:45 -0800

    Turned CartView into function component that uses custom hook

--- src/cart-view/CartView.js
+++ src/cart-view/CartView.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import {LicensePlate} from '../license-plate/LicensePlate';
 import {Jumbotron} from '../jumbotron/Jumbotron';
-import {getCartContents} from '../cart-service/cart-service';
-import {removeFromCart} from '../cart-service/cart-service';
+import {useCart} from "../cart-service/cart-hook";
 
-export class CartView extends React.Component {
 
-	state = {
-		cartContents: []
-	};
+export function CartView(props) {
 
-	async componentDidMount() {
-		let cartContents = await getCartContents();
-		this.setState({cartContents : cartContents});
-	}
+	const [cartContents] = useCart();
 
-	 removeItemFromCart = (plate) => {
+	const removeItemFromCart = (plate) => {
 		// TODO
 	}
 
-	render() {
-		let rows;
-		if (this.state.cartContents) {
-			rows = this.state.cartContents.map((licensePlate, index) => {
-				return (
-					<div key={licensePlate._id} className="col-md-4" style={{backgroundColor: (index % 2 === 0) ? '#F5F5F5' : ''}}>
-						<LicensePlate currency={this.props.currency} plate={licensePlate} buttonText="Remove from cart &times;" />
-					</div>
-				);
-			});
-		}
-		return (
-			<>
-				<Jumbotron title="My Cart" description="Your current cart contents:"/>
-				<div className="container">
-					{rows && rows.length === 0 && <div className="alert alert-info" role="alert">Your cart is empty</div>}
-					<div className="row" >
-						{rows}
-					</div>
-					<hr/>
+	let rows;
+	if (cartContents) {
+		rows = cartContents.map((licensePlate, index) => {
+			return (
+				<div key={licensePlate._id} className="col-md-4" style={{backgroundColor: (index % 2 === 0) ? '#F5F5F5' : ''}}>
+					<LicensePlate currency={props.currency} plate={licensePlate} buttonText="Remove from cart &times;" />
 				</div>
-			</>
-		);
+			);
+		});
 	}
+	return (
+		<>
+			<Jumbotron title="My Cart" description="Your current cart contents:"/>
+			<div className="container">
+				{rows && rows.length === 0 && <div className="alert alert-info" role="alert">Your cart is empty</div>}
+				<div className="row" >
+					{rows}
+				</div>
+				<hr/>
+			</div>
+		</>
+	);
 }
 
